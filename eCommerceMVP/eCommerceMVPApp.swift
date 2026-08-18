c249164fefd146975c569bd07217a0134eaa7cc3
Víctor Cedeño <contacto@victorcedeno.mx>
2026-08-18 13:50:00 -0600

    Initial project configuration

---
+++ eCommerceMVP/eCommerceMVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerceMVPApp.swift
+//  eCommerceMVP
+//
+//  Created by Víctor Cedeño on 18/08/26.
+//
+
+import SwiftUI
+
+@main
+struct eCommerceMVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
